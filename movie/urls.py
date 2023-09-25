a4d4356551d1f9bdc5bfcbcf55039550e5f6f90f
TrangLe <letrangle2002@gmail.com>
2023-09-25 21:47:43 +0700

    feat: create-FE

---
+++ movie/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('blog/', views.blog, name='blog'),
+    path('blog_detail', views.blog_detail, name='blog_detail'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
+]
