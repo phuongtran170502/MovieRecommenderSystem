6c19a135e176a0a06eacdb6b4d5db22aa99e133a
Phương Trần Khánh <102200314@sv1.dut.udn.vn>
2023-09-27 20:40:34 +0700

    feature/create-adminPage

--- movie/urls.py
+++ movie/urls.py
@@ -2,10 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #User
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('blog/', views.blog, name='blog'),
     path('blog_detail', views.blog_detail, name='blog_detail'),
     path('services/', views.services, name='services'),
     path('contact/', views.contact, name='contact'),
+    #Admin
+    path('general/', views.general, name='general'),
+    path('general/blank/', views.blank, name='blank'),
+    path('general/button/', views.button, name='button'),
+    path('general/chart/', views.chart, name='chart'),
+    path('general/element/', views.element, name='element'),
+    path('general/form/', views.form, name='form'),
+    path('general/table/', views.table, name='table'),
+    path('general/typography/', views.typography, name='typography'),
+    path('general/widget/', views.widget, name='widget'),
+    path('general/signin/', views.signin, name='signin'),
+    path('general/signup/', views.signup, name='signup'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Views of User
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'User/index.html')
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'User/about.html')
 def blog(request):
-    return render(request, 'blog.html')
+    return render(request, 'User/blog.html')
 def blog_detail(request):
-    return render(request, 'blog_detail.html')
+    return render(request, 'User/blog_detail.html')
 def services(request):
-    return render(request, 'services.html')
+    return render(request, 'User/services.html')
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'User/contact.html')
+
+# Views of Admin
+def general(request):
+    return render(request, 'Admin/general.html')
+def blank(request):
+    return render(request, 'Admin/blank.html')
+def button(request):
+    return render(request, 'Admin/button.html')
+def chart(request):
+    return render(request, 'Admin/chart.html')
+def element(request):
+    return render(request, 'Admin/element.html')
+def form(request):
+    return render(request, 'Admin/form.html')
+def table(request):
+    return render(request, 'Admin/table.html')
+def typography(request):
+    return render(request, 'Admin/typography.html')
+def widget(request):
+    return render(request, 'Admin/widget.html')
+def signin(request):
+    return render(request, 'Admin/signin.html')
+def signup(request):
+    return render(request, 'Admin/signup.html')
